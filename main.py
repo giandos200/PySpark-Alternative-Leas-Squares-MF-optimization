6c3d915e522f39d6840a2528afd17984872911cb
Giandomenico Cornacchia <60853532+giandos200@users.noreply.github.com>
2022-11-27 12:27:47 +0100

    Update main.py

--- main.py
+++ main.py
@@ -8,13 +8,6 @@ from src.preprocessing.Sampling import Sampling
 from src.preprocessing.Kcore import Kcore
 from src.model import trainModel,evaluateModel, top_movies
 from pyspark.sql.functions import *
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
 # Press the green button in the gutter to run the script.
